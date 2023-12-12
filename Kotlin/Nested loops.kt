3336ae66a8be983b88ef105d7a6623681f4d79be
charles <ccacharlesagnew@aol.com>
2023-12-12 12:03:01 -0500

    ideaj

---
+++ Kotlin/Nested loops.kt
@@ -0,0 +1,12 @@
+fun main() {
+    for (i in 1..10){
+        val numberOfSpaces = i-1
+        for (j in 1 ..numberOfSpaces) {
+            print(" ")
+        }
+        val numberOfStars = 11 - i
+        for (j in 1.. numberOfStars){
+            print("*")}
+        println()
+    }
+}
